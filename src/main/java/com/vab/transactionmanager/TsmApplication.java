4a77b8d21c3175897eb364198900c5a58145370b
unknown <vaibhavvergil3@gmail.com>
2025-06-18 00:48:16 +0530

    HTML Template update

--- src/main/java/com/vab/transactionmanager/TsmApplication.java
+++ src/main/java/com/vab/transactionmanager/TsmApplication.java
@@ -3,10 +3,14 @@ package com.vab.transactionmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TsmApplication {
 
 	public static void main(String[] args) {
+
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+05:30"));
 		SpringApplication.run(TsmApplication.class, args);
 	}
 
